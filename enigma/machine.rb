a3ed48f610057abab452a125620560df7c837e56
Grant Colegate <blaknite@thelanbox.com.au>
2017-04-29 14:46:17 +0930

    refactor machine to be more dry

--- enigma/machine.rb
+++ enigma/machine.rb
@@ -7,91 +7,107 @@ module Enigma
       'day_key'        => 'AAA',
     }
 
+    attr_reader :rotors, :reflector, :plug_board, :day_key
+
     def initialize(options = {})
       options = DEFAULTS.merge(options)
 
-      @rotors = []
-      ring_settings = options['ring_settings'].split(' ').map{ |rs| rs.to_i - 1 }.reverse
-      options['rotor_order'].upcase.split(' ').reverse.each_with_index do |r, i|
-        @rotors << Enigma::Rotor.new(AVAILABLE_ROTORS[r], ring_settings[i])
-      end
+      ring_positions = options['ring_positions'].split(' ').map{ |rs| rs.to_i - 1 }
+      @rotors = options['rotors'].upcase.split(' ').map.with_index{ |type, i|
+        Enigma::Rotor.new(type, ring_positions[i])
+      }.reverse!
 
       @reflector = Enigma::Reflector.new
-      @plug_board = Enigma::PlugBoard.new(options['plug_board'])
       @plug_board = Enigma::PlugBoard.new(options['plug_pairs'])
+      @day_key = options['day_key']
 
-      @key = options['key']
-      reset_rotor_stepping
+      reset!
     end
 
     # encode a message using the day key and its unique key
-    def encode(string)
-      message_key, message_content = split_key_and_message(string)
+    def encode(message)
+      message = message.upcase
 
-      encoded_message_key = message_key.chars.map{ |c| encode_character(c) }.join
-      set_rotor_stepping(message_key[0..2])
+      unique_key, content = split_key_and_content(message)
 
-      return encoded_message_key + message_content.chars.map{ |c| encode_character(c) }.join
-        .chars.each_with_index.map{ |c, idx| idx % 5 == 0 ? ' ' + c : c }.join
+      apply_key(unique_key)
+
+      content = group_characters(encode_string(content))
+
+      reset!
+
+      encode_string(unique_key) + content
     end
 
     # decode a message using the day key and its unique key
-    def decode(string)
-      message_key, message_content = split_key_and_message(string)
+    def decode(message)
+      message = message.upcase
 
-      decoded_message_key = message_key.chars.map{ |c| encode_character(c) }.join
-      set_rotor_stepping(decoded_message_key[0..2])
+      unique_key, content = split_key_and_content(message)
 
-      return decoded_message_key + ' ' + message_content.chars.map{ |c| encode_character(c) }.join
+      unique_key = encode_string(unique_key)
+
+      apply_key(unique_key)
+
+      content = group_characters(encode_string(content))
+
+      unique_key + content
     end
 
-    # sets the rotor stepping to match a given key
-    def set_rotor_stepping(key)
-      key.chars.reverse.each_with_index{ |c, i| @rotors[i].stepping = ALPHABET.index(c) }
-    end
-
-    # resets the rotor stepping to the 'day key'
-    def reset_rotor_stepping
-      set_rotor_stepping(@key)
+    def reset!
+      apply_key(day_key)
     end
 
     private
 
-    def encode_character(c)
-      c = c.upcase
+    # applies the given key to the machine's rotors
+    def apply_key(key)
+      fail StandardError, 'key length and rotor count does not match' unless key.length == rotors.length
+      key.chars.reverse_each.with_index{ |c, i| rotors[i].step_count = ALPHABET.index(c) }
+    end
 
+    # split the message and its unique key - the first word of the message is the key
+    def split_key_and_content(string)
+      string = string.split(' ')
+      [string[0], string[1..-1].join(' ')]
+    end
+
+    def encode_string(string)
+      string.chars.map{ |c| encode_character(c) }.join
+    end
+
+    def encode_character(c)
       return '' unless ALPHABET.include?(c) # only A-Z are valid characters in an encoded message
 
       # step the rotors before encoding each character
-      @rotors[2].step if @rotors[1].step_next_rotor?
-      @rotors[1].step if @rotors[1].step_next_rotor? || @rotors[0].step_next_rotor?
-      @rotors[0].step
+      rotors[2].step if rotors[1].step_next_rotor?
+      rotors[1].step if rotors[1].step_next_rotor? || rotors[0].step_next_rotor?
+      rotors[0].step
 
       # encode using plugboard
-      c = @plug_board.convert(c)
+      c = plug_board.encode(c)
 
       # encode forward through each rotor
-      c = @rotors.reduce(c) do |c, r|
-        r.forward(c, @rotors[@rotors.index(r) - 1] ? @rotors[@rotors.index(r) - 1].offset : 0)
-      end
+      c = rotors.reduce(c) { |c, r| r.forward_encode(c, safe_rotor_offset(r)) }
 
       # reflect the character back through the rotors
-      c = @reflector.reflect(c, @rotors[2].offset)
+      c = reflector.reflect(c, rotors.last.offset)
 
       # encode reverse through all the rotors
-      c = @rotors.reverse.reduce(c) do |c, r|
-        r.reverse(c, @rotors[@rotors.index(r) - 1] ? @rotors[@rotors.index(r) - 1].offset : 0)
-      end
+      c = rotors.reverse.reduce(c) { |c, r| r.reverse_encode(c, safe_rotor_offset(r)) }
 
       # encode again using plugboard
-      c = @plug_board.convert(c)
+      c = plug_board.encode(c)
 
-      return c
+      c
     end
 
-    # split the message and its unique key because they come in the format "aaaaaa this is a test"
-    def split_key_and_message(string)
-      [string.slice(0..5).upcase, string.slice(6..-1).strip.upcase]
+    def group_characters(string)
+      string.chars.map.with_index{ |c, i| i % 5 == 0 ? ' ' + c : c }.join
+    end
+
+    def safe_rotor_offset(rotor)
+      rotors[rotors.index(rotor) - 1] ? rotors[rotors.index(rotor) - 1].offset : 0
     end
   end
 end
