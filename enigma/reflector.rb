59d177626890526abc0ea79c2bcdcfb733117d6c
Grant Colegate <blaknite@thelanbox.com.au>
2017-04-29 14:45:23 +0930

    move reflector default to a constant

--- enigma/reflector.rb
+++ enigma/reflector.rb
@@ -1,13 +1,15 @@
 module Enigma
   class Reflector
+    DEFAULT_WIRES = 'YRUHQSLDPXNGOKMIEBFZCWVJAT'
+
     def initialize(wires = nil)
-      @wires = wires || 'YRUHQSLDPXNGOKMIEBFZCWVJAT'
+      @wires = wires || DEFAULT_WIRES
     end
 
     def reflect(c, offset = 0)
       c = @wires[(ALPHABET.index(c) - offset) % ALPHABET.length]
       c = ALPHABET[(ALPHABET.index(c) + offset) % ALPHABET.length]
-      return c
+      c
     end
   end
 end
