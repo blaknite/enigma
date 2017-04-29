721bd54b697c5997e5aee8a1fbc88ad824c80d45
Grant Colegate <blaknite@thelanbox.com.au>
2017-04-29 14:45:54 +0930

    rename rotor variables for better understanding

--- enigma/rotor.rb
+++ enigma/rotor.rb
@@ -1,40 +1,40 @@
 module Enigma
   class Rotor
-    attr_accessor :wires, :notch, :stepping, :ring_setting
+    attr_accessor :wires, :notch, :step_count, :ring_position
 
-    def initialize(rotor_settings, ring_setting)
+    def initialize(type, ring_position)
       # apply wire configuration and adjust for ring setting
-      self.wires = rotor_settings[:wires].chars.rotate(ring_setting).join
-      self.ring_setting = ring_setting
-      self.notch = rotor_settings[:notch]
-      self.stepping = 0
+      self.wires = AVAILABLE_ROTORS[type][:wires].chars.rotate(ring_position).join
+      self.ring_position = ring_position
+      self.notch = AVAILABLE_ROTORS[type][:notch]
+      self.step_count = 0
     end
 
     # encode character from right to left of rotor
     # offset comes from parent rotor
-    def forward(c, offset = 0)
-      self.wires[(ALPHABET.index(c) + self.stepping - offset) % ALPHABET.length]
+    def forward_encode(c, offset)
+      self.wires[(ALPHABET.index(c) + self.step_count - offset) % ALPHABET.length]
     end
 
     # encode character from left to right of rotor
     # offset comes from parent rotor
-    def reverse(c, offset = 0)
-      ALPHABET[(self.wires.index(c) - self.stepping + offset) % ALPHABET.length]
+    def reverse_encode(c, offset)
+      ALPHABET[(self.wires.index(c) - self.step_count + offset) % ALPHABET.length]
     end
 
     # rotate the rotor one step
     def step
-      self.stepping += 1
+      self.step_count += 1
     end
 
     # check if this rotor's position is at its notch
     def step_next_rotor?
-      ALPHABET[self.stepping % ALPHABET.length] == self.notch
+      ALPHABET[self.step_count % ALPHABET.length] == self.notch
     end
 
     # the current offset from A
     def offset
-      self.stepping - self.ring_setting
+      self.step_count - self.ring_position
     end
   end
 end
