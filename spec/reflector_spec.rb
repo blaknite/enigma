1457bbe800cb8c68f440bea3ce80cda050fbaacc
Grant Colegate <blaknite@thelanbox.com.au>
2017-05-01 21:49:19 +0930

    Add test for Reflector

---
+++ spec/reflector_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Enigma::Reflector do
+  let(:reflector) { Enigma::Reflector.new(type) }
+  let(:type) { 'B' }
+
+  describe '#reflect' do
+    it 'should return the reflected index' do
+      expect(reflector.reflect(0)).to eq Enigma::ALPHABET.index('Y')
+    end
+  end
+end
