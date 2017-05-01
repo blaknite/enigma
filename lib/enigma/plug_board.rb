d1d67fe92937b544cae00419d2b5ced5de599292
Grant Colegate <blaknite@thelanbox.com.au>
2017-05-01 12:30:54 +0930

    ensure only one plug can be assigned per socket

--- lib/enigma/plug_board.rb
+++ lib/enigma/plug_board.rb
@@ -1,11 +1,26 @@
 module Enigma
   class PlugBoard
     def initialize(plug_pairs)
-      @plug_pairs = Hash[*plug_pairs.upcase.split(' ').flat_map{ |pp| [pp[0], pp[1], pp[1], pp[0]] }]
+      @plug_pairs = build_plug_pairs(plug_pairs)
     end
 
     def encode(c)
       @plug_pairs[c] ? @plug_pairs[c] : c
+    end
+
+    private
+
+    def build_plug_pairs(plug_pairs)
+      plug_pairs.upcase.split(' ').each.with_object({}) do |plug_pair, plug_pairs|
+        build_plug_pair(plug_pair, plug_pairs)
+      end
+    end
+
+    def build_plug_pair(plug_pair, plug_pairs)
+      fail StandardError, "plug already connected to #{plug_pair[0]}" if plug_pairs.key?(plug_pair[0])
+      fail StandardError, "plug already connected to #{plug_pair[1]}" if plug_pairs.key?(plug_pair[1])
+      plug_pairs[plug_pair[0]] = plug_pair[1]
+      plug_pairs[plug_pair[1]] = plug_pair[0]
     end
   end
 end
